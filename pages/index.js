657d54210f528b4e686db3f0eca7e341c0d46ce1
JoniAguero <jaguero@trimix.com.ar>
2018-11-12 15:34:25 -0300

    Data from api.audiboom

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,69 @@
-import React, { Component } from 'react'
+import 'isomorphic-fetch'
+import Link from 'next/link'
 
-export default class index extends Component {
-  render() {
-    return (
-      <div>
-        <h1>¡Hola mundo!</h1>
+export default class extends React.Component {
 
-        <img src="/static/platzi-logo.png" alt="Logo Platzi" />
+    static async getInitialProps() {
+        let req = await fetch('https://api.audioboom.com/channels/recommended')
+        let { body: channels } = await req.json()
+        return { channels }
+    }
 
-        <style jsx>{`
-            h1 {
-                color: red
-            }
-        `}</style>
+    render() {
+        const { channels } = this.props
 
-        <style jsx global>{`
-            body {
-                background-color: yellow
-            }
-        `}</style>
-      </div>
-    )
-  }
+        return <div>
+            <header>Podcasts</header>
+
+            <div className="channels">
+                {channels.map((channel) => (
+                    <a className="channel" key={channel.id}>
+                        <img src={channel.urls.logo_image.original} alt="" />
+                        <h2>{channel.title}</h2>
+                    </a>
+                ))}
+            </div>
+
+            <style jsx>{`
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+        }
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+        a.channel {
+          display: block;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          width: 100%;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
+        }
+      `}</style>
+
+            <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: system-ui;
+          background: white;
+        }
+      `}</style>
+        </div>
+    }
 }
